8127a75baf905f866ffadf1293f3879189b04164
Jon Leighton <j@jonathanleighton.com>
2009-01-10 22:22:23 +0000

    mount_board_lines.rb - should take into account overlap of photo and mount board

--- mount_board_lines.rb
+++ mount_board_lines.rb
@@ -19,6 +19,13 @@ STANDARD_SIZES = {
   "8x12" => Size.new(203, 305)
 }
 
+def accumulate(array)
+  array.inject([]) do |mem, val|
+    mem << (mem.last || 0) + val
+    mem
+  end
+end
+
 print "Enter a WxH size in mm such as '142x204' or one of the standard sizes: #{STANDARD_SIZES.keys.join(", ")}: "
 size = gets.chomp
 
@@ -31,8 +38,14 @@ end
 
 print "Enter the border width in mm: "
 border_width = gets.chomp.to_i
-puts
 
+print "By how many mms should each edge of the photo overlap the mount board? "
+overlap = gets.chomp.to_i
+
+horizontal = accumulate([0, border_width, size.width  - (2 * overlap), border_width])
+vertical   = accumulate([0, border_width, size.height - (2 * overlap), border_width])
+
+puts
 puts "Markings need to be made at the following positions:"
-puts "Horizontal edges: 0, #{border_width}, #{border_width + size.width}, #{border_width + size.width + border_width}"
-puts "Vertical edges:   0, #{border_width}, #{border_width + size.height}, #{border_width + size.height + border_width}"
+puts "Horizontal edges: " + horizontal.join(", ")
+puts "Vertical edges:   " + vertical.join(", ")
